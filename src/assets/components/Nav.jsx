f9b2439181ab2ab86de04fff626d314d87ff8518
Athistaraja <athisraja45@gmail.com>
2024-03-12 21:49:09 +0530

    reinitialized files

---
+++ src/assets/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function nav() {
+    return (
+        <nav class="navbar navbar-light bg-light static-top">
+            <div class="container">
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <a class="btn btn-primary" href="#signup">Sign Up</a>
+            </div>
+        </nav>
+
+    )
+}
+
+export default nav
